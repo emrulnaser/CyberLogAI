5d2c669f9baa510759caa2af54f0c410088850a1
Emrul Naser <emruln@gmail.com>
2025-07-05 19:54:18 +0600

    Fix for Render deployment: use 0.0.0.0 and PORT

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def index():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))  # Render will set this
+    app.run(host='0.0.0.0', port=port)         # Host must be 0.0.0.0
